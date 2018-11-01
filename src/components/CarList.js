616399d50c8686326e218b684a77bc3d1b764874
Zubair Tariq <zubair_success747@yahoo.com>
2018-11-01 11:45:57 +0200

    Added some extra functionalities like Updating and Deleting a car and showing snackBar

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,21 +3,50 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import Snackbar from '@material-ui/core/Snackbar';
 import Fade from '@material-ui/core/Fade';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
+import SaveIcon from '@material-ui/icons/Save';
 import Addcar from './Addcar.js';
 
 class Carlist extends Component {
 
     constructor(params) {
         super(params);
-        this.state = {cars: [], showSnack: false};
+        this.state = {cars: [], showSnack: false, msg:''};
     }
 
     componentDidMount() {
         this.listCars();
     }
 
+    updateCar = (car, link) => {
+        fetch(link,
+        {method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(car)
+        })
+        .then(response => {
+            this.setState({showSnack: true, msg:'Car Saved'})
+            this.listCars();
+        })
+    }
+    renderEditable =(cellInfo) => {
+        return (
+          <div
+            style={{ backgroundColor: "#fafafa" }}
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={e => {
+              const data = [...this.state.cars];
+              data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+              this.setState({ cars: data });
+            }}
+            dangerouslySetInnerHTML={{
+              __html: this.state.cars[cellInfo.index][cellInfo.column.id]
+            }}
+          />
+        );
+      }
     handleClose = () => {
         this.setState({showSnack: false});
     };
@@ -34,7 +63,7 @@ class Carlist extends Component {
         fetch(link, {method: 'DELETE'})
         .then(response => {
             this.listCars();
-            this.setState({showSnack: true});
+            this.setState({showSnack: true, msg: 'Car Deleted'});
         })
     }
 
@@ -45,48 +74,59 @@ class Carlist extends Component {
         body: JSON.stringify(car)
         })
         .then(response => {
+            
             this.listCars();
+        })
+        .catch(err => {
+            console.error(err);
+            this.setState({showSnack: true, msg: 'Error in Saving'})
         })
     }
 
     render() {
         const columns = [{
             Header: 'Brand',
-            accessor: 'brand'
+            accessor: 'brand',
+            Cell: this.renderEditable
         }, {
             Header: 'Model',
             accessor: 'model',
+            Cell: this.renderEditable
             
         }, {
             Header: 'Year',
             accessor: 'year',
+            Cell: this.renderEditable
             
         }, {
             Header: 'Color',
             accessor: 'color',
+            Cell: this.renderEditable
             
         }, {
             Header: 'Fuel',
             accessor: 'fuel',
+            Cell: this.renderEditable
             
         }, {
             Header: 'Price €',
             accessor: 'price',
+            Cell: this.renderEditable
             
         }, {
+            Header: '',
             accessor: '_links.self.href',
             filterable: false,
             sortable: false,
-            Cell: ({value}) => {
-            return <div>
-                <IconButton
-                    aria-label="Delete"
-                    onClick={() => this.deleteCar(value)}>
-                    <DeleteIcon />
-                </IconButton>
-            </div>;
-            }
-        }];
+            Cell: ({row, value}) => (
+                <div><Button size="small" color="default" onClick={() => this.updateCar(row, value)}><SaveIcon /></Button><Button size="small" color="default" onClick={() => this.deleteCar(value)}><DeleteIcon /></Button>
+                </div>
+                ),
+            
+            }, 
+        ]
+        
+         
         return (
             <div>
                 <Addcar saveCar={this.saveCar} />
@@ -101,7 +141,7 @@ class Carlist extends Component {
                     open={this.state.showSnack}
                     onClose={this.handleClose}
                     TransitionComponent={Fade}
-                    message={<span id="message-id">Car deleted!</span>}
+                    message={this.state.msg}
                 />
           </div>
         );
