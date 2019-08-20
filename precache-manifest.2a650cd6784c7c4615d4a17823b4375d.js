cf46b23f8804b38d21f44048dd3aac8a900f6c53
Zubair Tariq <zubair_success747@yahoo.com>
2019-08-21 00:19:34 +0300

    Updates

---
+++ precache-manifest.2a650cd6784c7c4615d4a17823b4375d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9b250f37f8d82e312d4",
+    "url": "/CarShopApp/static/js/runtime~main.f9b250f3.js"
+  },
+  {
+    "revision": "739281354f1d84c9acc8",
+    "url": "/CarShopApp/static/js/main.73928135.chunk.js"
+  },
+  {
+    "revision": "224cbec9610b5624fe5f",
+    "url": "/CarShopApp/static/js/1.224cbec9.chunk.js"
+  },
+  {
+    "revision": "739281354f1d84c9acc8",
+    "url": "/CarShopApp/static/css/main.9e3616fc.chunk.css"
+  },
+  {
+    "revision": "224cbec9610b5624fe5f",
+    "url": "/CarShopApp/static/css/1.c7f2ac9d.chunk.css"
+  },
+  {
+    "revision": "a7e92a2141c57aeb6ebf83f5a6d10b2c",
+    "url": "/CarShopApp/index.html"
+  }
+];
